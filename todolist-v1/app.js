0886730f1f56f526f52a3ee74d67177ce53bba42
Kiranpal Singh <kiranpalsingh1806@gmail.com>
2021-11-16 13:17:56 +0530

    Add files

--- todolist-v1/app.js
+++ todolist-v1/app.js
@@ -10,40 +10,16 @@ app.set("view engine", "ejs");
 app.get("/", function(req, res) {
     var day = "";
     var today = new Date();
+
+    var options = {
+        weekday : "long",
+        day: "numeric",
+        month: "long"
+    };
+
+    var day = today.toLocaleDateString("en-US", options);
+
     var currentDay = today.getDay();
-
-    if(currentDay === 6 || currentDay === 0) {
-        day = "Weekend";
-    } else {
-        day = "Weekday";
-    }
-
-    switch (currentDay) {
-        case 0:
-            day = "Sunday";
-            break;
-        case 1:
-            day = "Monday";
-            break;
-        case 2:
-            day = "Tuesday";
-            break;
-        case 3:
-            day = "Wednesday";
-            break;
-        case 4:
-            day = "Thursday";
-            break;
-        case 5:
-            day = "Friday";
-            break;
-        case 6:
-            day = "Saturday";
-            break;
-
-        default:
-            console.log("Error: Current Day is equal to" + currentDay);
-    }
 
     res.render("list", {kindOfDay: day});
 });

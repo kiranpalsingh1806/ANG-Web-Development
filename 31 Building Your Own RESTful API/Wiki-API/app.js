11e47e8b41ac304e15a284abbd963a0799bc08ba
Kiranpal Singh <kiranpalsingh1806@gmail.com>
2022-01-13 19:12:09 +0530

    Add Starter Code for Restful API

---
+++ 31 Building Your Own RESTful API/Wiki-API/app.js
@@ -0,0 +1,34 @@
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require('mongoose');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(express.static("public"));
+
+mongoose.connect("mongodb://localhost:27017/wikiDB");
+
+const articleSchema = {
+    title: String,
+    content: String
+}
+
+const Article = mongoose.model("Article", articleSchema);
+
+app.get("/", function (req, res) {
+    res.send("<div> Hello World </div>")
+})
+
+//TODO
+
+app.listen(3000, function () {
+    console.log("Server started on port 3000");
+});

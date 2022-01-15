14959f7b80b251957cca897e387132e1393c95a4
Kiranpal Singh <kiranpalsingh1806@gmail.com>
2022-01-15 11:56:41 +0530

    Add GET and POST request to WIKI DB

--- 31 Building Your Own RESTful API/Wiki-API/app.js
+++ 31 Building Your Own RESTful API/Wiki-API/app.js
@@ -27,7 +27,30 @@ app.get("/", function (req, res) {
     res.send("<div> Hello World </div>")
 })
 
-//TODO
+app.get("/articles", function (req, res) {
+    Article.find(function (err, foundArticles) {
+        if (!err) {
+            res.send(foundArticles);
+        } else {
+            res.send(err);
+        }
+    });
+});
+
+app.post("/articles", function (req, res) {
+    const newArticle = new Article({
+        title: req.body.title,
+        content: req.body.content
+    });
+
+    newArticle.save(function (err) {
+        if (!err) {
+            res.send("Succesfully added a new article");
+        } else {
+            res.send(err);
+        }
+    });
+})
 
 app.listen(3000, function () {
     console.log("Server started on port 3000");

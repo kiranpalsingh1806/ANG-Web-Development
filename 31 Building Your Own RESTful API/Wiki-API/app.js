7ca343a431a38b5f502bf952683f4f645a14942f
Kiranpal Singh <kiranpalsingh1806@gmail.com>
2022-01-17 08:47:45 +0530

    Add Chained Route Handlers in Express

--- 31 Building Your Own RESTful API/Wiki-API/app.js
+++ 31 Building Your Own RESTful API/Wiki-API/app.js
@@ -27,40 +27,39 @@ app.get("/", function (req, res) {
     res.send("<div> Hello World </div>")
 })
 
-app.get("/articles", function (req, res) {
-    Article.find(function (err, foundArticles) {
-        if (!err) {
-            res.send(foundArticles);
-        } else {
-            res.send(err);
-        }
-    });
-});
+app.route("/articles")
+    .get(function (req, res) {
+        Article.find(function (err, foundArticles) {
+            if (!err) {
+                res.send(foundArticles);
+            } else {
+                res.send(err);
+            }
+        });
+    })
+    .post(function (req, res) {
+        const newArticle = new Article({
+            title: req.body.title,
+            content: req.body.content
+        });
 
-app.post("/articles", function (req, res) {
-    const newArticle = new Article({
-        title: req.body.title,
-        content: req.body.content
+        newArticle.save(function (err) {
+            if (!err) {
+                res.send("Succesfully added a new article");
+            } else {
+                res.send(err);
+            }
+        });
+    })
+    .delete(function (req, res) {
+        Article.deleteMany({}, function (err) {
+            if (!err) {
+                res.send("Succesfully deleted all the items");
+            } else {
+                res.send(err);
+            }
+        });
     });
-
-    newArticle.save(function (err) {
-        if (!err) {
-            res.send("Succesfully added a new article");
-        } else {
-            res.send(err);
-        }
-    });
-});
-
-app.delete("/articles", function (req, res) {
-    Article.deleteMany({}, function (err) {
-        if (!err) {
-            res.send("Succesfully deleted all the items");
-        } else {
-            res.send(err);
-        }
-    });
-})
 
 app.listen(3000, function () {
     console.log("Server started on port 3000");
